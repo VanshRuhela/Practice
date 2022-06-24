290379f38f1538419a02ffd6914dd42794581a29
VanshRuhela <vanshruhelavansh@gmail.com>
2022-06-25 03:30:45 +0530

    Binary Palindrome Prob Description

---
+++ Graph/GraphTraversals/binaryPalindrome.cpp
@@ -0,0 +1,24 @@
+/*=============================================================================
+#  PROBLEM:
+Construct binary palindrome by repeated appending and
+trimming.
+Given n and k, Construct a palindrome of size n using a binary number
+of size k repeating itself to wrap into the palindrome. The palindrome must
+always begin with 1 and contains maximum number of zeros. Examples :
+
+Input : n = 5,  k = 3
+Output : 11011
+Explanation : the 3 sized substring is
+110 combined twice and trimming the extra
+0 in the end to give 11011.
+
+Input : n = 2,  k = 8
+Output : 11
+Explanation : the 8 sized substring is 11......
+wrapped to two places to give 11.
+
+=============================================================================*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { return 0; }
