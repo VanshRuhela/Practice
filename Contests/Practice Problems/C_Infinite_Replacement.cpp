c70048330d244bbcb7da491c18ee60bcc8e02634
VanshRuhela <vanshruhelavansh@gmail.com>
2022-07-16 14:40:08 +0530

    Practice Prob

---
+++ Contests/Practice Problems/C_Infinite_Replacement.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define re(i, a, n) for (int i = a; i <= n; ++i)
+#define repr(i, a, n) for (int i = a; i >= n; --i)
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+using namespace std;
+
+void solve() {
+    string s;
+    string t;
+    cin >> s >> t;
+    if (t.size() == 1 && t[0] == 'a') {
+        cout << "1\n";
+        return;
+    }
+    if (t.find('a') != string::npos) {
+        cout << "-1\n";
+        return;
+    }
+
+    cout<<pow(2, s.size());
+
+}
+
+int main() {
+    fast_cin();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
