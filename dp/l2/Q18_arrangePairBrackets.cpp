9e1444d8cc0459f30b052817d44b35a85cc23455
VanshRuhela <vanshruhelavansh@gmail.com>
2022-07-20 18:23:07 +0530

    Arrange Pair of brackets - Catalan Nums

---
+++ dp/l2/Q18_arrangePairBrackets.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define re(i, a, n) for (int i = a; i <= n; ++i)
+#define repr(i, a, n) for (int i = a; i >= n; --i)
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+using namespace std;
+
+int main() {
+    fast_cin();
+    int n;
+    cin >> n;
+    int dp[n + 1];
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        int inside = i - 1;
+        int outside = 0;
+        while (inside >= 0) {
+            dp[i] += dp[inside] * dp[outside];
+            inside--;
+            outside++;
+        }
+    }
+
+    cout << dp[n] << endl;
+    return 0;
+}
