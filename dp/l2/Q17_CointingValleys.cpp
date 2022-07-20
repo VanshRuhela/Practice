b1354f42716014f48d49395d51697c7586ef448e
VanshRuhela <vanshruhelavansh@gmail.com>
2022-07-20 18:19:49 +0530

    code fix

--- dp/l2/Q17_CointingValleys.cpp
+++ dp/l2/Q17_CointingValleys.cpp
@@ -15,10 +15,22 @@ typedef long double ld;
 #define s second
 using namespace std;
 int main() {
-    fast_cin();
-    int t;
-    cin >> t;
-    while (t--) {
+    int n;
+    cin >> n;
+    int dp[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        int inside = i - 1;
+        int outside = 0;
+
+        while (inside >= 0) {
+            dp[i] += dp[outside] * dp[inside];
+            inside--;
+            outside++;
+        }
     }
+    cout << dp[n];
     return 0;
 }
