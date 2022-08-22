4bb735da5018ded6f1aa36152044301afe7b20e7
VanshRuhela <vanshruhelavansh@gmail.com>
2022-08-22 17:58:26 +0530

    optimized dsu

--- Graph/dsu/cycleDetection.cpp
+++ Graph/dsu/cycleDetection.cpp
@@ -15,14 +15,19 @@ class Graph {
     int findSet(int i, int parent[]) {
         if (parent[i] == -1) return i;
 
-        return findSet(parent[i], parent);
+        return parent[i] = findSet(parent[i], parent);
     }
 
-    void unionSet(int x, int y, int parent[]) {
+    void unionSet(int x, int y, int parent[], int rank[]) {
         int s1 = findSet(x, parent);
         int s2 = findSet(y, parent);
 
-        if (s1 != s2) parent[s2] = s1;
+        if (s1 != s2) {
+            if (rank[s1] < rank[s2])
+                parent[s1] = s2, rank[s2] += rank[s1];
+            else
+                parent[s2] = s1, rank[s1] += rank[s2];
+        }
     }
 
     bool contains_cycle() {
@@ -30,7 +35,11 @@ class Graph {
 
         // make everyone as their own parent i.e. -1
         int *parent = new int[V];
-        for (int i = 0; i < V; i++) parent[i] = -1;
+        int *rank = new int[V];
+        for (int i = 0; i < V; i++) {
+            parent[i] = -1;
+            rank[i] = 1;
+        }
 
         // iterate over the edge list
         for (auto edge : l) {
@@ -41,7 +50,7 @@ class Graph {
             int s2 = findSet(j, parent);
 
             if (s1 != s2)
-                unionSet(s1, s2, parent);
+                unionSet(s1, s2, parent, rank);
 
             else
                 return true;
@@ -51,16 +60,15 @@ class Graph {
     }
 };
 
-
-int main(){
+int main() {
     Graph g(5);
-    g.addEdge(0,1);
-    g.addEdge(1,2);
-    g.addEdge(2,3);
-    g.addEdge(3,4);
-    g.addEdge(4,1);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+    g.addEdge(4, 1);
 
-    cout << g.contains_cycle()<< endl;
+    cout << g.contains_cycle() << endl;
 
     return 0;
 }
